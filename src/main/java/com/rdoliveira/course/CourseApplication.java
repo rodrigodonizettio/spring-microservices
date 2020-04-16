10e29f5fff6f8b100d7689606dab4c3cc693ac5e
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-04-15 23:52:29 -0300

    Adding missing header for main application class

--- src/main/java/com/rdoliveira/course/CourseApplication.java
+++ src/main/java/com/rdoliveira/course/CourseApplication.java
@@ -3,6 +3,10 @@ package com.rdoliveira.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author rodrigodonizettio
+ */
+
 @SpringBootApplication
 public class CourseApplication {
 
